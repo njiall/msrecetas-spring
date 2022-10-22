8ff9075a50f90b9b894511c0ed586c51fb31d4bf
Ruben <ruben.astudillo@gmail.com>
2022-10-22 08:14:53 +0200

    Seguimos con el pto Cors

--- src/main/java/com/recetario/controller/RecetaController.java
+++ src/main/java/com/recetario/controller/RecetaController.java
@@ -11,13 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.recetario.repository.entities.Receta;
 import com.recetario.service.RecetaService;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET})
 @RequestMapping("receta")
 public class RecetaController {
 
@@ -35,7 +36,6 @@ public class RecetaController {
 		return responseEntity;
 	}
 	
-	@CrossOrigin(origins = "*")
 	@GetMapping("detalle/{id}")
 	public ResponseEntity<?> obtieneDetalleReceta(@PathVariable Long id) {
 		ResponseEntity<?> responseEntity = null;
