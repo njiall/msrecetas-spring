f2636a5cfa7eb601448b463698e289d5834369a5
Ruben <ruben.astudillo@gmail.com>
2022-10-23 11:54:12 +0200

    Probamos método JSONP

--- src/main/java/com/recetario/controller/RecetaController.java
+++ src/main/java/com/recetario/controller/RecetaController.java
@@ -1,11 +1,7 @@
 package com.recetario.controller;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.recetario.repository.entities.Receta;
 import com.recetario.service.RecetaService;
 
@@ -31,10 +23,8 @@ public class RecetaController {
 	@Autowired
 	RecetaService servicio;
 	
-	@Autowired
-	ObjectMapper om;//mapeador JSON
-
 	@GetMapping
+	@CrossOrigin(origins = "*")
 	public ResponseEntity<?> listarRecetas() {
 		ResponseEntity<?> responseEntity = null;
 		Iterable<Receta> listadoCursos = null;
@@ -46,6 +36,7 @@ public class RecetaController {
 	}
 	
 	@GetMapping("detalle/{id}")
+	@CrossOrigin(origins = "*")
 	public ResponseEntity<?> obtieneDetalleReceta(@PathVariable Long id) {
 		ResponseEntity<?> responseEntity = null;
 		Optional<Receta> optional;
